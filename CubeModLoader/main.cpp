bc2b30c85ac725a7ed9c13ff440264a6629a6ac8
Gildo Junior <1gildojunior@gmail.com>
2026-08-18 02:57:43 -0300

    fix: Restore can equip item method override

--- CubeModLoader/main.cpp
+++ CubeModLoader/main.cpp
@@ -76,7 +76,7 @@ void SetupHandlers() {
     setup_function(cube__Creature__OnPlayerDrownDeath);
     setup_function(cube__Creature__OnPlayerFallDeath);
     setup_function(cube__Creature__OnCreatureDeath);
-    // setup_function(cube__Creature__CanEquipItem);
+    setup_function(cube__Creature__CanEquipItem);
     // setup_function(cube__StartMenuWidget__Draw);
     setup_function(cube__Game__Update);
     setup_function(cube__Item__GetBuyingPrice);
